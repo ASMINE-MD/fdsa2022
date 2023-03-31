771e705cc144720cac3e152e463289139a8bf8cb
ASMINE-MD <84783667+ASMINE-MD@users.noreply.github.com>
2023-03-31 20:18:19 +0530

    Initial commit

---
+++ basics2.js
@@ -0,0 +1,13 @@
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// // }
+// let i=2;
+// do{
+//     console.log(i);
+// }while (i>2) 
+let students=['asmine','asna','aliya']
+for(let student of students){
+    console.log(`Hello ${student}`);
+}
